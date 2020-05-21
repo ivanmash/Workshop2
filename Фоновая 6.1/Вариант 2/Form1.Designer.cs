6e36d54b14d2e0fdd1c93db8ddda65069a092f8a
ivanmash <iv.mashkarin@yandex.ru>
2020-05-21 09:45:39 +0300

    Update

---
+++ Фоновая 6.1/Вариант 2/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Вариант_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(187, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Location = new System.Drawing.Point(337, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "B";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(467, 316);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Фоновая 6.1/Вариант 2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Вариант_2
+{
+    public partial class Form1 : Form
+    {
+        int x; int w; int rast1;
+        int y; int h; int rast2;
+        int width;
+        public Form1()
+        {
+            InitializeComponent();
+            x = button2.Left;
+            y = button2.Top;
+            width = button1.Width;
+            h = this.ClientSize.Height;
+            w = this.ClientSize.Width;
+            rast1 = this.ClientSize.Width - button2.Right;
+            rast2 = this.ClientSize.Height - button2.Top - button2.Height;
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (rast2 > 0 && rast1 > 0)
+            {
+                if (rast2 >= 4 && rast1 >= 2)
+                {
+                    button2.Top = button2.Top + 4;
+                    button2.Left = button2.Left + 2;
+                    rast1 = rast1 - 2;
+                    rast2 = rast2 - 4;
+                }
+                if(rast2 < 4)
+                {
+                    button2.Top = button2.Top + this.ClientSize.Height - button2.Top - button2.Height;
+                    rast2 = 0;
+                }
+                if (rast1 < 2)
+                {
+                    button2.Left = button2.Left + (this.ClientSize.Width - button2.Right);
+                    rast1 = 0;
+                }
+            }
+            else
+            {
+                button2.Location = new Point(x, y);
+                rast1 = this.ClientSize.Width - button2.Right;
+                rast2 = this.ClientSize.Height - button2.Top - button2.Height;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (button1.Width <= 50)
+                button1.Width = width;
+            else button1.Width = button1.Width - 2;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            button2.Location = new Point(x, y);
+            w = w + 2;
+            h = h + 2;
+            this.Size = new System.Drawing.Size(w, h);
+            rast1 = this.ClientSize.Width - button2.Right;
+            rast2 = this.ClientSize.Height - button2.Top - button2.Height;
+        }
+    }
+}
